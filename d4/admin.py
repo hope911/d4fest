699bdeaf3e548a7be3812d80cee2205a39610d43
hope911 <72310026+hope911@users.noreply.github.com>
2021-07-15 21:41:17 -0400

    Create admin.py

---
+++ d4/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from bang import models
+
+admin.site.register(models.Account)
+admin.site.register(models.Member)
+admin.site.register(models.Card)
+admin.site.register(models.Event)
+admin.site.register(models.Song)
+admin.site.register(models.Gacha)
+admin.site.register(models.Rerun)
+admin.site.register(models.Item)
+admin.site.register(models.Area)
+admin.site.register(models.AreaItem)
+admin.site.register(models.Asset)
+admin.site.register(models.Costume)
